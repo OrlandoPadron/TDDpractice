4c7c69c67d66a4a8e07a4d3e49050e850d5e8015
Orlando <prueba>
2019-01-23 12:52:06 +0000

    Merging branches

--- src/test/java/PracticingBranchesTest.java
+++ src/test/java/PracticingBranchesTest.java
@@ -1,8 +1,10 @@
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PracticingBranchesTest {
     @Test
     public void given_an_empty_branch(){
-        
+        assertThat(0).isZero();
     }
 }
